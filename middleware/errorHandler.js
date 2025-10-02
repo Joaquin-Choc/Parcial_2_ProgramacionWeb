349a49969ee0c7186be63205719b1ef01aeb45f6
Joaquin-Choc <123263923+Joaquin-Choc@users.noreply.github.com>
2025-10-01 20:51:43 -0600

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,10 +1,14 @@
 const errorHandler = (err, req, res, next) => {
-    console.error('Error inesperado:', err);
-    
-    res.status(500).json({
-        error: "Error interno del servidor",
-        message: process.env.NODE_ENV === 'development' ? err.message : 'Algo salió mal'
-    });
-}
+  console.error('Error inesperado:', err);
+  
+  if (res.headersSent) {
+    return next(err);
+  }
+  
+  res.status(500).json({
+    error: "Error interno del servidor",
+    message: process.env.NODE_ENV === 'development' ? err.message : 'Algo salió mal'
+  });
+};
 
 module.exports = errorHandler;
